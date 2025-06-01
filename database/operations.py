5d54d0cdad010853ded6e26dcad908762afd432d
Chris Linnell <chrislinnell232@gmail.com>
2025-06-01 18:01:50 +0100

    Reworked user retrieval and create/get message functions

--- database/operations.py
+++ database/operations.py
@@ -1,19 +1,28 @@
+from datetime import datetime
+from typing import Any
+
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
 from database.models import EncryptedMessage, User
 
-async def get_user_id(engine: AsyncEngine, public_key: str) -> int:
-    """Retrieves or creates a user id for the supplied public key."""
+async def get_or_create_user(engine: AsyncEngine, public_key: str) -> User:
+    """Retrieves or creates a user for the supplied public key."""
+    statement = select(User).where(User.public_key == public_key)
     async with AsyncSession(engine, expire_on_commit=False) as session:
         statement = select(User).where(User.public_key == public_key)
-        result = await session.scalars(statement)
-        user = result.one_or_none()
+        user = await session.scalar(statement)
         if user is None:
             user = User(public_key=public_key)
             session.add(user)
-            await session.commit()
-        return user.id
+            try:
+                await session.commit()
+            except IntegrityError:
+                await session.rollback()
+                result = await session.scalars(statement)
+                user = result.one()
+        return user
     
 async def create_message(
         engine: AsyncEngine,
@@ -22,15 +31,38 @@ async def create_message(
         sender_key: str,
         recipient_key: str,
     ) -> EncryptedMessage:
-    sender_id = await get_user_id(engine, sender_key)
-    recipient_id = await get_user_id(engine, recipient_key)
+    sender = await get_or_create_user(engine, sender_key)
+    recipient = await get_or_create_user(engine, recipient_key)
     async with AsyncSession(engine, expire_on_commit=False) as session:
         message = EncryptedMessage(
             encrypted_text=encrypted_text,
             signature=signature,
-            sender_id=sender_id,
-            recipient_id=recipient_id,
+            sender_id=sender.id,
+            recipient_id=recipient.id,
         )
         session.add(message)
         await session.commit()
         return message
+
+async def get_messages(
+        engine: AsyncEngine,
+        recipient_key: str,
+        min_datetime: datetime | None = None,
+    ) -> list[dict[str, Any]]:
+    user = await get_or_create_user(engine, recipient_key)
+    statement = select(EncryptedMessage)
+    statement = statement.where(EncryptedMessage.recipient_id == user.id)
+    if min_datetime is not None:
+        statement = statement.where(EncryptedMessage.timestamp >= min_datetime)
+    async with AsyncSession(engine) as session:
+        messages = await session.scalars(statement)
+        result: list[dict[str, Any]] = [
+            {
+                'sender_key': message.sender.public_key,
+                'encrypted_text': message.encrypted_text,
+                'signature': message.signature,
+                'timestamp': message.timestamp,
+            }
+            for message in messages
+        ]
+        return result
