38c3d706b4c1f1d22bdb84618e6fe105994b54b9
Chris Linnell <chrislinnell232@gmail.com>
2025-06-01 18:00:07 +0100

    Archived files

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 # VALIDATE KEYS AND SIGNATURES HERE
 
+import binascii
 import os
 
 from base64 import b64decode, b64encode, urlsafe_b64decode, urlsafe_b64encode
 from contextlib import asynccontextmanager
+from datetime import datetime
 from typing import Annotated, Any
 
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, Query
-from pydantic import AfterValidator, BaseModel, Field, StringConstraints
+from pydantic import AfterValidator, BaseModel, Field, ValidationError
 from sqlalchemy.ext.asyncio import create_async_engine
 
-from database.operations import create_message
+from database import operations
 from database.models import Base
+
+def is_base64(value: str) -> str:
+    try:
+        urlsafe_b64decode(value)
+    except binascii.Error:
+        raise ValueError('String is not valid Base64')
+    return value
+
 
 # Set key constants.
 MAX_PLAINTEXT_LENGTH = 2000
@@ -46,6 +56,7 @@ class BaseRequestModel(BaseModel):
             max_length=44,
             min_length=44,
         ),
+        AfterValidator(is_base64),
     ]
 
 # Define the model for all responses.
@@ -72,7 +83,36 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-class OutgoingMessage(BaseRequestModel):
+class MessageRetrievalModel(BaseRequestModel):
+    min_datetime: Annotated[
+        datetime | None,
+        Field(
+            description=(
+                'An optional datetime that filters out messages posted '
+                'any earlier than the supplied value. This is intended to '
+                'avoid unneccessarily repeating retrievals.'
+            ),
+            default=None,
+        ),
+    ]
+
+@app.post("/messages/retrieve")
+async def retrieve_messages(request: MessageRetrievalModel):
+    retrieved_messages = await operations.get_messages(
+        engine=engine,
+        recipient_key=request.public_key,
+        min_datetime=request.min_datetime,
+    )
+    return Response(
+        status='success',
+        message=f'{len(retrieved_messages)} messages retrieved.',
+        data={
+            'messages': retrieved_messages,
+        }
+    )
+
+
+class OutgoingMessageModel(BaseRequestModel):
     recipient_public_key: Annotated[
         str,
         Field(
@@ -85,6 +125,7 @@ class OutgoingMessage(BaseRequestModel):
             max_length=44,
             min_length=44,
         ),
+        AfterValidator(is_base64),
     ]
     encrypted_text: Annotated[
         str,
@@ -95,7 +136,7 @@ class OutgoingMessage(BaseRequestModel):
             ),
             max_length=MAX_CIPHERTEXT_LENGTH,
             examples=[urlsafe_b64encode(b'Lorem ipsum dolor sit amet...')],
-        )
+        ),
     ]
     signature: Annotated[
         str,
@@ -108,13 +149,13 @@ class OutgoingMessage(BaseRequestModel):
             max_length=88,
             min_length=88,
             examples=[urlsafe_b64encode(b'zyxwvuts' * 8)],
-        )
+        ),
+        AfterValidator(is_base64),
     ]
 
-
 @app.post("/messages/send")
-async def post_message(message: OutgoingMessage):
-    posted_message = await create_message(
+async def post_message(message: OutgoingMessageModel):
+    posted_message = await operations.create_message(
         engine=engine,
         encrypted_text=message.encrypted_text,
         signature=message.signature,
@@ -125,16 +166,9 @@ async def post_message(message: OutgoingMessage):
         status='success',
         message='Message sent.',
         data={
-            'timestamp': posted_message.timestamp,
+            'message_timestamp': posted_message.timestamp,
         }
     )
-
-    return {
-        'sender': sender_id,
-        'recipient': recipient_id,
-    }
-
-#app = FastAPI()
 
 @app.post("/")
 async def read_root1(body: BaseRequestModel):
